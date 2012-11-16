55883ec85ab343e893250e68562ec54ed1d14f74
Haukur Rosinkranz <haukurr11@ru.is>
2012-11-16 18:40:19 +0000

    Pusha stuffinu

--- target/scala-2.9.1/src_managed/main/views/html/board/viewboard.template.scala
+++ target/scala-2.9.1/src_managed/main/views/html/board/viewboard.template.scala
@@ -21,35 +21,36 @@ import com.avaje.ebean._
 import play.mvc.Http.Context.Implicit._
 import views.html._
 /**/
-object viewboard extends BaseScalaTemplate[play.api.templates.Html,Format[play.api.templates.Html]](play.api.templates.HtmlFormat) with play.api.templates.Template1[$greater[$less[java.util.List, Object], <error>],play.api.templates.Html] {
+object viewboard extends BaseScalaTemplate[play.api.templates.Html,Format[play.api.templates.Html]](play.api.templates.HtmlFormat) with play.api.templates.Template1[viewmodels.IndexModel,play.api.templates.Html] {
 
     /**/
-    def apply/*1.6*/(stuff: java.util.List<Object>):play.api.templates.Html = {
+    def apply/*1.6*/(stuff: viewmodels.IndexModel):play.api.templates.Html = {
         _display_ {
 
-Seq[Any](format.raw/*1.37*/("""
+Seq[Any](format.raw/*1.36*/("""
     """),_display_(Seq[Any](/*2.6*/main(Html("RuPin"))/*2.25*/ {_display_(Seq[Any](format.raw/*2.27*/("""
 
+    <h2>Welcome! """),_display_(Seq[Any](/*4.19*/stuff/*4.24*/.getUser().getUsername())),format.raw/*4.48*/("""</h2>
     <p>
         This is a test!
     </p>
     """)))})))}
     }
     
-    def render(stuff:$greater[$less[java.util.List, Object], <error>]) = apply(stuff)
+    def render(stuff:viewmodels.IndexModel) = apply(stuff)
     
-    def f:(($greater[$less[java.util.List, Object], <error>]) => play.api.templates.Html) = (stuff) => apply(stuff)
+    def f:((viewmodels.IndexModel) => play.api.templates.Html) = (stuff) => apply(stuff)
     
     def ref = this
 
 }
                 /*
                     -- GENERATED --
-                    DATE: Fri Nov 16 14:08:53 GMT 2012
+                    DATE: Fri Nov 16 18:37:15 GMT 2012
                     SOURCE: /home/haukur/skil4_honn/RuPin/app/views/board/viewboard.scala.html
-                    HASH: d2cf875e5f2db0d8329b293faa61a88712521917
-                    MATRIX: 807->5|915->36|955->42|982->61|1021->63
-                    LINES: 27->1|30->1|31->2|31->2|31->2
+                    HASH: 4439e7cc6ff37438e126d627ceec60e50860b592
+                    MATRIX: 780->5|887->35|927->41|954->60|993->62|1048->82|1061->87|1106->111
+                    LINES: 27->1|30->1|31->2|31->2|31->2|33->4|33->4|33->4
                     -- GENERATED --
                 */
             
